02e1595edfd9c9d52b48a42e44c98eac2f45fb52
sergey27564 <sergey.sorokin8913@gmail.com>
2017-01-29 21:37:28 +0300

    v0.2.1d

--- bot.py
+++ bot.py
@@ -9,18 +9,19 @@ nowday = config.wd[datetime.now().strftime("%A, %d. %B %Y %I:%M%p").split()[0][0
 
 print(nowday)
 
+
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
 	bot.send_message(message.chat.id, config.start)
-	print(message.chat.id)
+	
 
 @bot.message_handler(commands=['help'])
 def handle_help(message):
 	markup = telebot.types.ReplyKeyboardMarkup()
 	markup.row('Репозиторий лямбды на гите', 'Номер телефона Хайзенберга')
-	markup.row()
+	if str(message.chat.id) == str(64634999): markup.row('Я из М4О-111Б')
 	bot.send_message(message.chat.id, 'Что ты хочешь узнать?', reply_markup=markup)
 
 @bot.message_handler(content_types=["text"])
@@ -37,7 +38,7 @@ def repeat_all_messages(message):
 	elif 'О боте' in message.text or 'о боте' in message.text:
 		bot.send.message(message.chat.id, 'Бота написал Сорокин Сергей с ФРЭЛА на языке Python3. Потому что могу.')
 	
-	elif 'М4О-111Б' in message.text and message.chat.id == 64634999:		
+	elif 'М4О-111Б' in message.text:		
 		markup = telebot.types.ReplyKeyboardMarkup()
 		markup.row('Расписание на сегодня', 'Расписание на завтра')
 		markup.row('Когда лаба?', 'Какая пара следующая?')
